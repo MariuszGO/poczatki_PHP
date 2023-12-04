8f0577cf628a7f96ad73d1339973191eba7af778
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-12-04 14:39:54 +0100

    Update index.php
    n2

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
-echo "Witaj Świecie";
+$a = 100;
+$b = 100;
+
+$c = "100ss";
+$d = "100ss";
+
+
+echo $a + $b;
+
+echo "<br><br>";
+
+echo $c . $d;
+
+
+
+echo "To jest integer: " . $a . ", a to jest string b: $b <br> ";
+echo "To jest $a <br><br><br>";
+
+echo "A jest typu :".  gettype($a) . "<br>";
+echo "B jest typu :" . gettype($b);
+
+
+
+
 ?>
+
     
