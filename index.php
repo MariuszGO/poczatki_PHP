9e7afcc61dc1a7fc555b5d08ce34ca656b6a91a2
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-12-11 14:17:59 +0100

    aa
    11.12.2023

--- index.php
+++ index.php
@@ -1,39 +1,6 @@
 <?php
 
-
-    for($i = 0; $i <= 100; $i++)
-    {
-        $tablica_liczb[$i] = true;
-    }
-
-   $j = 2;
-
-    for($i = 2; $i<=Sqrt($n);$i++)
-    {
-        if ($tablica_liczb[$i] == true)
-        {
-            for($j = $i*$i; $j<=$n;$j=$j+$i)
-            {
-                $tablica_liczb[$j] = false;
-            }
-        }
-
-    }
-
-    
-
-
-
-        for ($i = 2; $i <=100; $i++)
-        {
-            if ($tablica_liczb[$i] == true)
-            {
-                echo $i. " ";
-            }
-        }
-
-
-
+echo "Imie: " .  $_POST["im"] . " i nazwisko: " . $_POST["na"];
 
 
 ?>
